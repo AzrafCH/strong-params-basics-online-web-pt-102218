c6f3130bed87e969e8b410ea293b6528fe291e72
Azraf Chowdhury <achowdhur@ltu.edu>
2019-03-25 09:42:34 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
 	def create
 	  @post = Post.new(post_params(:title, :description))
 	  @post.save
